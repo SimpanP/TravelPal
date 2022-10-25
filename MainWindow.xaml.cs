914a6a9b1d90d7ddaa68683db75dd4f7dababc11
SimpanP <perssonsimon98@gmail.com>
2022-10-25 19:07:55 +0200

    added all info on main window
    Added all info and design for all text and buttons on main window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TRAVELPAL
 {
@@ -23,6 +12,55 @@ namespace TRAVELPAL
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextUserName_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            tbUsername.Focus();
+        }
+
+        private void TbUsername_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tbUsername.Text) && tbUsername.Text.Length > 0)
+            {
+                textUserName.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textUserName.Visibility = Visibility.Visible;
+            }
+        }
+
+
+
+
+        private void TextPassword_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            tbPassword.Focus();
+        }
+
+
+
+        private void TbPassword_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tbPassword.Password) && tbPassword.Password.Length > 0)
+            {
+                textPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
