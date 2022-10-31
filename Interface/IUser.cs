05d3b67b03f93fa1c55fa6f6023cc4bf5069e86a
SimpanP <113165536+SimpanP@users.noreply.github.com>
2022-10-31 11:57:47 +0100

    buttons, interface, and classes

---
+++ Interface/IUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRAVELPAL.Interface
+{
+    internal interface IUser
+    {
+    }
+}
